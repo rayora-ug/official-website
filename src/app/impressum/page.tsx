d1f9ac533a8e433722a5c4dd8180797eb911c089
rayora-ug <contact@rayora.de>
2026-06-10 14:09:09 +0200

    Add Impressum page with company details

---
+++ src/app/impressum/page.tsx
@@ -0,0 +1,32 @@
+export default function ImpressumPage() {
+  return (
+    <main style={{ maxWidth: "800px", margin: "40px auto", padding: "20px" }}>
+      <h1>Impressum</h1>
+
+      <p>
+        <strong>Rayora UG (haftungsbeschränkt)</strong>
+      </p>
+
+      <p>
+        Geschäftsführer:<br />
+        Mohammad Rayhanur Rahman
+      </p>
+
+      <p>
+        Samlandweg 90<br />
+        22415 Hamburg<br />
+        Deutschland
+      </p>
+
+      <p>
+        E-Mail: contact@rayora.de
+      </p>
+
+      <p>
+        Handelsregister:<br />
+        Amtsgericht Hamburg<br />
+        HRB 193273
+      </p>
+    </main>
+  );
+}
